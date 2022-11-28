c100beaa64e5d1c424788701896365c787f61d99
noltes8471 <97260654+noltes8471@users.noreply.github.com>
2022-11-28 17:41:26 -0600

    Update BlueSmthgIdk.java

--- BlueSmthgIdk.java
+++ BlueSmthgIdk.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.drive.opmode.personal;
+package org.firstinspires.ftc.teamcode.drive.opmode;
 import org.firstinspires.ftc.teamcode.drive.Robot;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
@@ -24,47 +24,52 @@ public class BlueSmthgIdk extends LinearOpMode {
     //If you turn outside a trajectory, the .end statement will be incorrect so you should do something like: traj1.end().plus(new Pose2d(0, 0, Math.toRadians(90)))
     //go backwards in a trajectory by adding a true after new Pose2d()
     //ex: Trajectory trajectory = drive.trajectoryBuilder(new Pose2d(), true)
-    
+
     private Robot autoRobot;
     public void runOpMode() throws InterruptedException {
         autoRobot = new Robot(hardwareMap);
         waitForStart();
 
         autoRobot.setPoseEstimate(new Pose2d());
+        //NOT BEING USED
+        //COLOR DETECTION
+        /*Trajectory colorDetectTraj = autoRobot.trajectoryBuilder(new Pose2d())
+                .forward(15)
+                .build();*/
 
-        Trajectory colorDetectTraj = autoRobot.trajectoryBuilder(new Pose2d())
-
-                .build();
         int parkValue = autoRobot.detectColor();
-        Trajectory MoveToStick = autoRobot.trajectoryBuilder(new Pose2d())
+
+
+        //MOVE TO STICK
+        Trajectory moveToStick = autoRobot.trajectoryBuilder(new Pose2d())
+                .splineToSplineHeading(new Pose2d(40, 40, Math.toRadians(45)), Math.toRadians(0))//coordinates are wrong
                 //go forward pushing the Team Element out of the way; lining up to go between the sticks 12in
                 //swerve right to the tall stick
                 //turn towards tall stick 45ish degrees
                 //forward (closing in the rest of the way)
                 .build();
 
-        Trajectory CappingTraj = autoRobot.trajectoryBuilder(new Pose2d())
-                .liftToHeightEncoders(4,0.7);
-                .openGrabber();
-                .liftToHeightEncoders(0,0.7);
-                .build();
 
-        Trajectory parkingTraj = autoRobot.trajectoryBuilder(new Pose2d())
+        //maybe precision forward with distance sensor
+        autoRobot.liftToHeightEncoders(4,0.7);
+        autoRobot.openGrabber();
+        autoRobot.liftToHeightEncoders(0,0.7);
 
-                .build();
-            //back up
-            //turnLeft 45 degrees
-            //depending on color sensor, go forward to parking
-            if (parkValue == 1) { //Quadrant 1
-                Trajectory moveToQuadrant2Traj = autoRobot.trajectoryBuilder(new Pose2d())
-                        //swerve left two tiles for quadrant 1
-                        .build();
-            } else if (parkValue == 2) { //Quadrant 2
-                Trajectory moveToQuadrant2Traj = autoRobot.trajectoryBuilder(new Pose2d())
-                        //swerve left one tile for quadrant 2
-                        .build();
-            } else if (parkValue == 3) { //Quadrant 3(already there)
-                //stay and do something like: System.out.println("celebrate!");
-            }
+        //back up
+        //turnLeft 45 degrees
+        //depending on color sensor, go forward to parking
+        if (parkValue == 1) { //Quadrant 1
+            Trajectory moveToQuadrant2Traj = autoRobot.trajectoryBuilder(moveToStick.end())
+                    .splineToSplineHeading(new Pose2d(40, 40, Math.toRadians(45)), Math.toRadians(0))//swerve left two tiles for quadrant 1
+                    .build();
+        } else if (parkValue == 2) { //Quadrant 2
+            Trajectory moveToQuadrant2Traj = autoRobot.trajectoryBuilder(moveToStick.end())
+                    .splineToSplineHeading(new Pose2d(40, 40, Math.toRadians(45)), Math.toRadians(0))//swerve left one tile for quadrant 2
+                    .build();
+        } else if (parkValue == 3) { //Quadrant 3(already there)
+            Trajectory moveToQuadrant3Traj = autoRobot.trajectoryBuilder(moveToStick.end())//stay and do something like: System.out.println("celebrate!");
+                    .splineToSplineHeading(new Pose2d(40, 40, Math.toRadians(45)), Math.toRadians(0))
+                    .build();
+        }
     }
 }
